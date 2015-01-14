a69dda20a078a6db39ad3811672e98ca533fa2e7
manojtd <manojtd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-14 13:08:23 +0000

    Preparing major update to V6.00
    git-svn-id: https://plugins.svn.wordpress.org/easy-paypal-lte/trunk@1067867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ezppCfg-WP.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbHost = DB_HOST ;
-$dbName = DB_NAME ;
-if (empty($GLOBALS['table_prefix'])) $table_prefix = 'wp_' ;
-else $table_prefix = $GLOBALS['table_prefix'] ;
-$dbPrefix = $table_prefix . "ezpp_" ;
-$dbUsr = DB_USER ;
-$dbPwd = DB_PASSWORD ;
-$dbEmail = "" ;
